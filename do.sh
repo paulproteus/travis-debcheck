54271da837b999bfb505ce874e10ae9b83978269
Asheesh Laroia <asheesh@asheesh.org>
2014-06-21 21:59:42 -0700

    Use paulproteus fork for now

--- do.sh
+++ do.sh
@@ -2,17 +2,30 @@
 set -e  # Fail on errors
 set -x  # Verbosity all the way
 
-EXTRA_GIT_BUILDPACKAGE_ARGS="--git-ignore-new"
+GIT_IGNORE_NEW="true"  # hack for now
+USE_ALIOTH="false"
 SKIP_PBUILDER=true
+
+## Pick which one to build -- the Asheesh fork, or the Alioth packaging
+GIT_URL="https://github.com/paulproteus/alpine-packaging.git"
+if [[ "$USE_ALIOTH" -eq "true" ]] ; then
+    GIT_URL="https://alioth.debian.org/anonscm/git/collab-maint/alpine.git"
+fi
+
+if [[ "$GIT_IGNORE_NEW" -eq "true" ]] ; then
+    EXTRA_GIT_BUILDPACKAGE_ARGS="--git-ignore-new"
+else
+    EXTRA_GIT_BUILDPACKAGE_ARGS=""
+fi
 
 sudo apt-get install git-buildpackage
 
 # Get latest alpine packaging
-git clone https://alioth.debian.org/anonscm/git/collab-maint/alpine.git
+git clone "$GIT_URL"
 
 # Make sure it builds outside a pbuilder
 cd alpine
-git-buildpackage $EXTRA_GIT_BUILDPACKAGE_ARGS
+git-buildpackage $EXTRA_GIT_BUILDPACKAGE_ARGS  # intentionally not quoted
 
 if [[ "$SKIP_PBUILDER" -eq "true" ]] ; then
     exit 0  # skip pbuilder for now
