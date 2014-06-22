53eb98b19254d50079982d6a53bc01803bf66637
Asheesh Laroia <asheesh@asheesh.org>
2014-06-22 00:53:26 -0700

    Hopefully also do a lintian check now

--- do.sh
+++ do.sh
@@ -79,5 +79,12 @@ sudo apt-get install ubuntu-dev-tools
 wget https://ftp-master.debian.org/keys/archive-key-7.0.asc
 gpg --import $PWD/archive-key-7.0.asc
 pbuilder-dist sid create --debootstrapopts --keyring=$HOME/.gnupg/pubring.gpg --mirror http://cdn.debian.net/debian/
+
+# Before building, add a hook to run lintian
+mkdir ~/pbuilderhooks
+cp /usr/share/doc/pbuilder/examples/B90lintian $HOME/pbuilderhooks
+echo "HOOKDIR=$HOME/pbuilderhooks/" >> ~/.pbuilderrc
+# FIXME: also run piuparts or something else???
+
 pbuilder-dist sid build ../*.dsc
 
