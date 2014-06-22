fd9cf1f1e27e46066eb486ae4da0f247456b09e7
Asheesh Laroia <asheesh@asheesh.org>
2014-06-21 21:37:09 -0700

    Create a pbuilder in which to do the building

--- do.sh
+++ do.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 sudo apt-get install git-buildpackage
 
+# Get latest alpine packaging
 git clone https://alioth.debian.org/anonscm/git/collab-maint/alpine.git
 
+# Make sure it builds outside a pbuilder
 cd alpine
 gbp buildpackage
+
+# Create a pbuilder chroot
+sudo apt-get install ubuntu-dev-tools
+pbuilder-dist sid create
+pbuilder-dist build ../*.dsc
 
