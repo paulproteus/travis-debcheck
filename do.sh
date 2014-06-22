a67fcf1d2d8d1c07da6951c40132e9f61592b559
Asheesh Laroia <asheesh@asheesh.org>
2014-06-21 23:20:29 -0700

    Use new (7.0/wheezy) keyring

--- do.sh
+++ do.sh
@@ -70,7 +70,8 @@ fi
 
 # Create a pbuilder chroot
 sudo apt-get install ubuntu-dev-tools
+wget https://ftp-master.debian.org/keys/archive-key-7.0.asc
 sudo apt-get install debian-archive-keyring
-pbuilder-dist sid create --debootstrapopts --keyring=/usr/share/keyrings/debian-archive-keyring.gpg
+pbuilder-dist sid create --debootstrapopts --keyring=$PWD/archive-key-7.0.asc
 pbuilder-dist sid build ../*.dsc
 
